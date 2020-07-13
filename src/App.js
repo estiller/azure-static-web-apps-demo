dab5acd394498303ad264ea3739b1fd29e19b28d
Eran Stiller <git@stiller.blog>
2020-07-13 22:17:22 +0300

    Add authn & authz

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HomePage from "./HomePage";
 import Header from "./Header";
 import { Route } from "react-router-dom";
 import BooksPage from "./BooksPage";
 
 function App() {
+  const [principal, setPrincipal] = useState(undefined);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("/.auth/me");
+      if (response.ok) {
+        const content = await response.json();
+        setPrincipal(content.clientPrincipal);
+      } else {
+        setPrincipal(null);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header principal={principal} />
       <Route exact path="/" component={HomePage} />
       <Route exact path="/books" component={BooksPage} />
     </>
--- src/Header.js
+++ src/Header.js
@@ -1,28 +1,43 @@
 import React from "react";
-import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import { Link, NavLink } from "react-router-dom";
 
-export default function Header() {
+export default function Header({ principal }) {
   return (
     <Navbar>
-      <NavItem>
-        <Navbar.Brand as={Link} to="/">
-          Library
-        </Navbar.Brand>
-      </NavItem>
+      <Navbar.Brand as={Link} to="/">
+        Library
+      </Navbar.Brand>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <NavItem>
-            <Nav.Link as={NavLink} to="/" activeClassName="active" exact>
-              Home
-            </Nav.Link>
-          </NavItem>
-          <NavItem>
-            <Nav.Link as={NavLink} to="/books" activeClassName="active">
-              Books
-            </Nav.Link>
-          </NavItem>
+          <Nav.Link as={NavLink} to="/" exact>
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/books">
+            Books
+          </Nav.Link>
         </Nav>
+      </Navbar.Collapse>
+      <Navbar.Collapse className="justify-content-end">
+        {(() => {
+          switch (principal) {
+            case undefined:
+              return;
+            case null:
+              return (
+                <Nav.Link href="/.auth/login/github">
+                  Login with GitHub
+                </Nav.Link>
+              );
+            default:
+              return (
+                <>
+                  <Navbar.Text>Hi {principal.userDetails}!</Navbar.Text>
+                  <Nav.Link href="/.auth/logout">Logout</Nav.Link>
+                </>
+              );
+          }
+        })()}
       </Navbar.Collapse>
     </Navbar>
   );
