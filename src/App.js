beed5e197f625bfe071e64b915ffd16fb0eb68b3
Eran Stiller <git@stiller.blog>
2020-07-19 18:51:05 +0300

    Remove the auth call from App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import HomePage from "./HomePage";
 import Header from "./Header";
 import { Route } from "react-router-dom";
 import BooksPage from "./BooksPage";
 
 function App() {
-  const [principal, setPrincipal] = useState(undefined);
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("/.auth/me");
-      if (response.ok) {
-        const content = await response.json();
-        setPrincipal(content.clientPrincipal);
-      } else {
-        setPrincipal(null);
-      }
-    }
-    fetchData();
-  }, []);
-
   return (
     <>
-      <Header principal={principal} />
+      <Header />
       <Route exact path="/" component={HomePage} />
       <Route exact path="/books" component={BooksPage} />
     </>
